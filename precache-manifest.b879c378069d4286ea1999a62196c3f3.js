7afa3efe7b642a441f3a0c9c33440cfbcd68edcf
Shi vam <shivamshashank44@gmail.com>
2022-03-20 03:54:18 +0530

    Updates

---
+++ precache-manifest.b879c378069d4286ea1999a62196c3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf91e29b358b032e0582b9c2f963c58a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20ab126e4af31a09bf07",
+    "url": "./static/css/main.85c5b7f1.chunk.css"
+  },
+  {
+    "revision": "91c93f0fb8bb702c1898",
+    "url": "./static/js/2.2872868f.chunk.js"
+  },
+  {
+    "revision": "13e061b2fd50c1d7f7b7e0bb533040d6",
+    "url": "./static/js/2.2872868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ab126e4af31a09bf07",
+    "url": "./static/js/main.c70918de.chunk.js"
+  },
+  {
+    "revision": "4b4a3dd7838700a0a037",
+    "url": "./static/js/runtime-main.ef0389d9.js"
+  }
+]);
